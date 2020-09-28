0659be92aed406d2d84df882d004bfb3f08d658f
Alexandr <48907709+AlexandrMel@users.noreply.github.com>
2020-09-28 15:10:57 +0200

    Added Update Post Route  and method
    Added update.php route and update method in Post Class,

--- models/Post.php
+++ models/Post.php
@@ -75,5 +75,75 @@ public function read_single(){
     $this->category_name = $row['category_name'];
 }
 
+//Creat Post
 
+public function create(){
+    //Create query
+
+$query = 'INSERT INTO ' .
+$this->table . '
+SET
+  title = :title,
+  body = :body,
+  author = :author,
+  category_id = :category_id';
+    //Prepare our statement
+    $stmt = $this->conn->prepare($query);
+    //Clean data
+    $this->title = htmlspecialchars(strip_tags($this->title));
+    $this->body = htmlspecialchars(strip_tags($this->body));
+    $this->author = htmlspecialchars(strip_tags($this->author));
+    $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+    //Bind Data
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':body', $this->body);
+    $stmt->bindParam(':author', $this->author);
+    $stmt->bindParam(':category_id', $this->category_id);
+//Execute query
+if($stmt->execute()){
+    return true;
+}
+//Print error if something goes wrong
+printf("Error: %s.\n", $stmt->error);
+return false;
+
+}
+
+//Update Post
+public function update(){
+    //Create query
+
+$query = 'UPDATE ' .
+$this->table . '
+SET
+  title = :title,
+  body = :body,
+  author = :author,
+  category_id = :category_id
+  WHERE id = :id';
+    //Prepare our statement
+    $stmt = $this->conn->prepare($query);
+    //Clean data
+    $this->title = htmlspecialchars(strip_tags($this->title));
+    $this->body = htmlspecialchars(strip_tags($this->body));
+    $this->author = htmlspecialchars(strip_tags($this->author));
+    $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    //Bind Data
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':body', $this->body);
+    $stmt->bindParam(':author', $this->author);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':id', $this->id);
+//Execute query
+if($stmt->execute()){
+    return true;
+}
+//Print error if something goes wrong
+printf("Error: %s.\n", $stmt->error);
+return false;
+
+}
 }
