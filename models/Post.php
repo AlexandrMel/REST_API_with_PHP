bb4f426a984cf9a17771e9e156a11eb2c3f65362
Alexandr <48907709+AlexandrMel@users.noreply.github.com>
2020-09-28 12:56:53 +0200

    Added read.php route to access all posts
    Created api/post/read.php, set headers, got the array of posts through Post+Databse Classes, encoded to JSON and returned

--- models/Post.php
+++ models/Post.php
@@ -16,7 +16,7 @@ public $created_at;
 
 //Contructor with DB
 public function __construct($db){
-    this->conn = $db;
+    $this->conn = $db;
 }
 
 //Get Posts
@@ -35,8 +35,8 @@ public function read(){
     categories c ON p.category_id = c.id
     ORDER BY
     p.created_at DESC';
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt;
 }
-$stmt = $this->conn->prepare($query);
-$stmt->execute();
-return $stmt;
 }
