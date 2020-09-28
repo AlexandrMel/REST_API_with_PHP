ba35c5f61db30c85f6e10621c8ca807600fe6a07
Alexandr <48907709+AlexandrMel@users.noreply.github.com>
2020-09-28 16:34:16 +0200

    Added Create and Read Category Routes
    Added Create and Read Category Routes

--- api/category/create.php
+++ api/category/create.php
@@ -1,35 +1,31 @@
 <?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  include_once '../../config/Database.php';
+  include_once '../../models/Category.php';
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
 
-//Import Classes
-include_once'../../config/Database.php';
-include_once'../../models/Post.php';
+  // Instantiate blog post object
+  $category = new Category($db);
 
-//Instantiate DB and connect
-$database = new Database();
-$db = $database->connect();
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
 
-//Instantiate blog post object
-$post = new Post($db);
+  $category->name = $data->name;
 
-//Get raw posted data
-$data = json_decode(file_get_contents("php://input"));
-
-//Copy data to post Class object
-$post->title = $data->title;
-$post->body = $data->body;
-$post->author= $data->author;
-$post->category_id = $data->category_id;
-
-//Create the Post
-if($post->create()){
-    echo json_encode(array(
-        'message'=> "Post Created"
-    ));
-} else {
-    echo json_encode(array("message"=> 'Post Not Created'));
-}
+  // Create Category
+  if($category->create()) {
+    echo json_encode(
+      array('message' => 'Category Created')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Category Not Created')
+    );
+  }
