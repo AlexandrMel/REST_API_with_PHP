d4047939274a5324cf305a90324576db32e292e5
Alexandr <48907709+AlexandrMel@users.noreply.github.com>
2020-09-28 13:42:19 +0200

    Added read_single post route
    Added Get one Post by ID from DB

--- models/Post.php
+++ models/Post.php
@@ -35,8 +35,45 @@ public function read(){
     categories c ON p.category_id = c.id
     ORDER BY
     p.created_at DESC';
+    //Prepare our statement
     $stmt = $this->conn->prepare($query);
+    //Execute the query
     $stmt->execute();
     return $stmt;
 }
+
+//Get Single Post
+public function read_single(){
+    $query = 'SELECT 
+    c.name as category_name,
+    p.id,
+    p.category_id,
+    p.title,
+    p.body,
+    p.author,
+    p.created_at
+    FROM
+    ' . $this->table . ' p
+    LEFT JOIN 
+    categories c ON p.category_id = c.id
+    WHERE
+    p.id = ?
+    LIMIT 0,1';
+    //Prepare our statement
+    $stmt = $this->conn->prepare($query);
+    //Bind ID
+    $stmt->bindParam(1, $this->id);
+    //Execute the query
+    $stmt->execute();
+    //Get the 1 table row with result and assign to the coresponding Class properties
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    //Set Properties
+    $this->title = $row['title'];
+    $this->body = $row['body'];
+    $this->author = $row['author'];
+    $this->category_id = $row['category_id'];
+    $this->category_name = $row['category_name'];
+}
+
+
 }
